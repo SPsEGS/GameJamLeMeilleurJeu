c323979222813334ab6bd7d94b1837e30974f44e
RainDeRiv <marie.bruyant@yahoo.fr>
2023-11-04 10:39:04 +0100

    crouch speed

--- Scripts/Human.gd
+++ Scripts/Human.gd
@@ -1,3 +1,21 @@
 extends Player
 
+var crouching : bool = false
+var speed_crouching = speed / 2
+var reel_speed = speed
 
+func input_loop() :
+	super()
+	if (Input.is_action_just_pressed("p1_crouch")) :
+		if(!crouching) :
+			crouch()
+			crouching = true
+		else :
+			speed = reel_speed
+			crouching = false
+			
+	
+	
+
+func crouch() :
+	speed = speed_crouching
