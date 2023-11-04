15d2215ade347f5801e3e5f6b31a5774a63db460
Ned <67144261+Ne-d@users.noreply.github.com>
2023-11-04 09:49:20 +0100

    Add gravity and jumping

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,19 +2,28 @@ extends CharacterBody2D
 
 class_name Player
 
-var direction : float = 0
+@export var speed : float = 80000
+@export var gravity : float = 3000
+@export var jump_impulse : float = 1000
 
-var SPEED : float = 1000
+var direction : float = 0
+var has_jumped : bool = false
 
 func input_loop() :
-	
 	direction = int(Input.is_action_pressed("p1_mv_right")) - int(Input.is_action_pressed("p1_mv_left"))
-	
+	if(Input.is_action_pressed("p1_jump") and is_on_floor() and !has_jumped):
+		jump()
+
 func move(delta) :
-	
-	velocity.x = direction * SPEED * delta
-	velocity.y = 0
+	velocity.x = direction * speed * delta
+	velocity.y += gravity * delta
 	move_and_slide()
+	if(is_on_floor()):
+		has_jumped = false
+
+func jump():
+	velocity.y = -jump_impulse
+	has_jumped = true
 
 func _process(delta):
 	input_loop()
