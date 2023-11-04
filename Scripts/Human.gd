0ef6618306ddd5d4509c6ee41fbc0f9207f90ecc
RainDeRiv <marie.bruyant@yahoo.fr>
2023-11-04 11:17:49 +0100

    crouch

--- Scripts/Human.gd
+++ Scripts/Human.gd
@@ -1,21 +1,25 @@
 extends Player
 
-var crouching : bool = false
-var speed_crouching = speed / 2
-var reel_speed = speed
-
-func input_loop() :
-	super()
-	if (Input.is_action_just_pressed("p1_crouch")) :
-		if(!crouching) :
-			crouch()
-			crouching = true
-		else :
-			speed = reel_speed
-			crouching = false
-			
-	
-	
-
-func crouch() :
-	speed = speed_crouching
+#var crouching : bool = false
+#var speed_crouching = speed / 2
+#var reel_speed = speed
+#
+#@onready var _animation_player = $AnimationPlayer
+#
+#func input_loop() :
+#	direction = int(Input.is_action_pressed("p1_mv_right")) - int(Input.is_action_pressed("p1_mv_left"))
+#	if(Input.is_action_pressed("p1_jump") and is_on_floor() and !has_jumped and !crouching):
+#		jump()
+#	if (Input.is_action_just_pressed("p1_crouch") and is_on_floor()) :
+#		if(!crouching) :
+##			crouch()
+#			crouching = true
+#		else :
+#			#speed = reel_speed
+#			crouching = false
+#			_animation_player.play("un_crouching")
+#
+#
+##func crouch() :
+#	speed = speed_crouching
+#	_animation_player.play("crouching")
